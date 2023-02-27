0a2094dbc4ba5ab4f1a82907719db5e5715ffcb0
ansachuk <ansachuk@icloud.com>
2023-02-27 20:47:31 +0200

    hw 6 ex 9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const hexColorSpanRef = document.querySelector(".color");
+const changeColorBtnRef = document.querySelector(".change-color");
+
+changeColorBtnRef.addEventListener("click", () => {
+  const currentHexColor = getRandomHexColor();
+
+  document.body.style.backgroundColor = currentHexColor;
+  hexColorSpanRef.textContent = currentHexColor;
+});
